eaa2a6f4c4f1edab402078cb5615d79e8a06cd84
Eva Kostadinova <e.ts.kostadinova@gmail.com>
2025-08-07 16:16:06 +0300

    Delete More08.CircleAreaAndParamemeter.js

--- More08.CircleAreaAndParamemeter.js
+++
@@ -1,9 +0,0 @@
-function circleAreaAndParameter(r){
-    let calculatedArea = r * r * Math.PI;
-    let calculatedParameter = (2 * r) * Math.PI;
-    
-    console.log(calculatedArea.toFixed(2));
-    console.log(calculatedParameter.toFixed(2));
-}
-
-circleAreaAndParameter(4.5);
