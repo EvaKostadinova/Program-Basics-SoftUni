88f0ff1f4fb1e5a031457487bec7bf565cdb1cf8
Eva Kostadinova <e.ts.kostadinova@gmail.com>
2025-08-07 16:14:07 +0300

    Delete Exercise04.VacationBooksList.js

--- Exercise04.VacationBooksList.js
+++
@@ -1,8 +0,0 @@
-function summerReading (pageCount, pagesPerHour, daysCount){
-    let totalHours = pageCount / pagesPerHour;
-    let hoursPerDay = totalHours / daysCount;
-
-    console.log(hoursPerDay);
-}
-
-summerReading (212, 20, 2);
