21b943cafda966fad0d2954334c0db014e7ae5f5
Eva Kostadinova <e.ts.kostadinova@gmail.com>
2025-08-07 16:13:58 +0300

    Delete Exercise03.DepositCalculator.js

--- Exercise03.DepositCalculator.js
+++
@@ -1,9 +0,0 @@
-function calculatorDeposit(deposit, timeInMonths, interest){
-    let sumInterest = deposit * (interest/100);
-    let interestForMonth = sumInterest/12;
-    let finalSum = deposit + timeInMonths * interestForMonth;
-
-    console.log (finalSum);
-}
-
-calculatorDeposit (2350, 6, 7);
