34919cfe2544a0621f94874bf6a44adad3c41976
Eva Kostadinova <e.ts.kostadinova@gmail.com>
2025-08-07 16:13:20 +0300

    Delete 08.PetShop.js

--- 08.PetShop.js
+++
@@ -1,8 +0,0 @@
-function expenses(dogsFood, catsFood){
-    let dogsFoodPrice = 2.5;
-    let catsFoodPrice = 4;
-    let allExpenses = (dogsFood * dogsFoodPrice) + (catsFood * catsFoodPrice);
-    console.log(allExpenses + " lv.")
-}
-
-expenses(13, 9);
